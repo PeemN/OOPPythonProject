16917b2301aec2deed2ff79ee3488661655fe021
PeemN <4astralp@gmail.com>
2016-12-22 14:32:23 +0700

    add class modelsprite

--- SrslyPongGame.py
+++ SrslyPongGame.py
@@ -5,35 +5,39 @@ from platform import BluePlatform, RedPlatform
  
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 600
+
+class ModelSprite(arcade.Sprite):
+      def __init__(self, *args, **kwargs):
+            self.model = kwargs.pop('model', None)
  
-class SrslyPongGame(arcade.Window):
+            super().__init__(*args, **kwargs)
+ 
+      def sync_with_model(self):
+            if self.model:
+                  self.set_position(self.model.x, self.model.y)
+ 
+      def draw(self):
+            self.sync_with_model()
+            super().draw()
+
+class SrslyPongGameWindow(arcade.Window):
       def __init__(self, width, height):
             super().__init__(width, height)
 
             arcade.set_background_color(arcade.color.BLACK)
 
-            self.bluePlatform_sprite = arcade.Sprite('images\BluePlatform.png')
-            self.redPlatform_sprite = arcade.Sprite('images\RedPlatform.png')
-            
-            self.creator = Creator(width, height) 
-            #self.bluePlatform = BluePlatform(25, 300)
-            #self.redPlatform = RedPlatform(975, 300)
+            self.creator = Creator(width, height)            
 
+            self.bluePlatform_sprite = ModelSprite('images\BluePlatform.png',model=self.creator.bluePlatform)
+            self.redPlatform_sprite = ModelSprite('images\RedPlatform.png',model=self.creator.redPlatform)
+            
       def on_draw(self):
             arcade.start_render()
             self.bluePlatform_sprite.draw()
             self.redPlatform_sprite.draw()
             
       def animate(self,delta):
-            #bluePlatform = self.bluePlatform
-            #redPlatform = self.redPlatform
-            #bluePlatform.animate(delta)
-            #redPlatform.animate(delta)
-            self.creator.animate(delta)
-            self.bluePlatform_sprite.set_position(self.creator.bluePlatform.x, self.creator.bluePlatform.y)
-            self.redPlatform_sprite.set_position(self.creator.redPlatform.x, self.creator.redPlatform.y)
-
-                  
+            self.creator.animate(delta)                  
 if __name__ == '__main__':
-      window = SrslyPongGame(SCREEN_WIDTH, SCREEN_HEIGHT)
+      window = SrslyPongGameWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
       arcade.run()
